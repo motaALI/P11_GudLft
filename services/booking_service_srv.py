3e15050bc18293f50842b671a6c42e2b78950342
motaALI <motawakil.ali.90@gmail.com>
2023-06-11 18:18:43 +0200

    [FIX]

--- services/booking_service_srv.py
+++ services/booking_service_srv.py
@@ -1,4 +1,4 @@
-
+maxAuthorathedToReserve = 12
 
 class bookingError(Exception):
     pass
@@ -7,6 +7,8 @@ def check_club_possibility_to_reserve_places(club, placesRequired):
     point_available = club['points']
     if placesRequired > int(point_available):
         raise bookingError(f"You don't have enough points. Points remaining ({point_available})!")
+    elif placesRequired > maxAuthorathedToReserve:
+        raise bookingError(f"You cannot reserve more than ({maxAuthorathedToReserve}) places!")
     else:
         return True 
     
